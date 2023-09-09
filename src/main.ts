278e6172dc68bc43f2ebeed305188108fd706824
XMMPPS <xmmpps@outlook.com>
2023-09-09 14:20:07 +0800

    Use jsdelivr to accelerate webfont

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import '@mdi/font/css/materialdesignicons.min.css';
 import App from './App.vue'
 import { registerPlugins } from '@/utils/plugins'
 
