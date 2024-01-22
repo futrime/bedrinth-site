97e78a6c2cf0bead969c28f9f86bae01724a8b3a
harry-xi <harry-xi@outlook.com>
2024-01-22 20:04:05 +0800

    style: Remove useless code

--- app/page.tsx
+++ app/page.tsx
@@ -15,18 +15,6 @@ export default async function Page ({
   return (
     <main>
       <div className="container mx-auto px-3 mt-24 pt-4">
-        {/* {result.items.map(result => (
-          <React.Fragment key={result.repoPath}>
-            <Link
-              href={`/${result.repoOwner}/${result.repoName}/${result.latestVersion}`}
-              className="block py-5 mx-2 transition hover:text-blue-600 hover:transition"
-            >
-              <h2 className="font-medium text-lg">{result.name}</h2>
-              <p className="text-sm text-gray-500">{result.description}</p>
-            </Link>
-            <div className="border-t-2" />
-          </React.Fragment>
-        ))} */}
         {result.items.map(pluginCard)}
       </div>
     </main>
